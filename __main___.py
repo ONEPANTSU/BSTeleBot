c5014f269587f1bd6ff9418c65fe32bdddad7c64
ONEPANTSU <conandet@mail.ru>
2022-05-20 23:39:18 +0300

    => AIOGRAM

--- __main___.py
+++ __main___.py
@@ -1,64 +1,17 @@
-import json
+import logging
+from aiogram import Bot, Dispatcher, executor, types
 
-from telegram.ext import Updater, CommandHandler
-from telegram.ext import MessageHandler, Filters, InlineQueryHandler
-import bsbot_config as config
-updater = Updater(token=config.API_TOKEN)
-dispatcher = updater.dispatcher
+from bsbot_config import API_TOKEN
 
-def about():
-    return "Для извлечения исходных данных используйте команды:" \
-           "/navigators index, где index - номер параметра в таблице навигаторов;"
 
-# функция обработки команды '/start'
-def func_start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Bonch-Science Bot!")
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+logging.basicConfig(level=logging.INFO)
 
-# функция обработки текстовых сообщений
-def func_text(update, context):
-    text_out = 'Получено сообщение: ' + update.message.text + '\n' + about()
-    context.bot.send_message(chat_id=update.effective_chat.id, text=text_out)
+@dp.message_handler(commands="Hello")
+async def cmd_test1(message: types.Message):
+    await message.reply("Hello! I'm Bonch-Science Bot!")
 
-# функция обработки команды '/navigators'
-def func_navigators(update, context):
-    if context.args:
-        text_reply = get_navigator_data(context.args[0])
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                text=text_reply)
-    else:
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                text='Необходимо указать аргумент команды /navigators')
 
-def get_navigator_data(self, index_param):
-    ret = json.dumps("Данные не найдены!( Параметр:{}", ensure_ascii=False)
-    index = int(index_param)
-    #..........................
-    return ret
-
-# функция обработки не распознанных команд
-def func_unknown(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Введена неизвестная команда! :c")
-
-# обработчик команды '/start'
-start_handler = CommandHandler('start', func_start)
-dispatcher.add_handler(start_handler)
-
-# обработчик текстовых сообщений
-text_handler = MessageHandler(Filters.text & (~Filters.command), func_text)
-dispatcher.add_handler(text_handler)
-
-# обработчик команды '/navigators'
-navigators_handler = CommandHandler('navigators', func_navigators)
-dispatcher.add_handler(navigators_handler)
-
-# обработчик не распознанных команд
-unknown_handler = MessageHandler(Filters.command, func_unknown)
-dispatcher.add_handler(unknown_handler)
-
-# запуск прослушивания сообщений
-updater.start_polling()
-
-# обработчик нажатия Ctrl+C
-updater.idle()
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
